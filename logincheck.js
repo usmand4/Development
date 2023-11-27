e9f36eb2b41a925f0180f6ca548b5c11c83c98fb
Usman Danish <usmanalid4@gmail.com>
2023-11-27 16:47:04 +0500

    add code file

---
+++ logincheck.js
@@ -0,0 +1,17 @@
+let userobject ={
+    user1:{
+        name: 'user1',
+        password: '123',
+        role: 'admin'
+    },
+    user2:{
+        name: 'user2',
+        password: '123',
+        role: 'manager'
+    },
+    user3:{
+        name: 'user3',
+        password: '123',
+        role: 'user'
+    },
+}
